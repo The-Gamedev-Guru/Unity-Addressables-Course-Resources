8b90d753818ca50a96b99ac477b4d07c4d78d354
rubentorresbonet <cofruben@gmail.com>
2019-12-27 12:55:47 +0100

    Adding addressables json loading support

---
+++ Assets/Scripts/AddressablesJsonLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace TheGamedevGuru
+{
+public class AddressablesJsonLoader : MonoBehaviour
+{
+    [SerializeField] private AssetReference jsonReference = null;
+    
+    IEnumerator Start()
+    {
+        var asyncOperation = jsonReference.LoadAssetAsync<TextAsset>();
+        yield return asyncOperation;
+        var jsonText = asyncOperation.Result.text;
+        Debug.Log("JSON content: " + jsonText);
+        //var jsonData = JsonUtility.FromJson<YourCustomJsonDataStructure>(jsonText);
+    }
+}
+}
